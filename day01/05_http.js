2708e1ffb9855a9108799a013fde38c4a80cda28
wmc425454438 <superwong0827@gmail.com>
2021-09-20 10:19:53 +0800

    feat: http模块

---
+++ day01/05_http.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const http = require('http')
+http.createServer((request, response) => {
+    // console.log('a request' ,getPrototypeChain(request));
+    // console.log('a response' ,getPrototypeChain(response));
+    // response.end('Hi Node');
+    // 语义化
+    // Best or nothing
+
+    // response = 'hahaha';
+    // response.body = 'heiheihei';
+
+
+    const { url , method } = request
+    if (url === '/' && method === 'GET') {
+        fs.readFile('index.html', (err, data) => {
+            if(err) {
+                response.writeHead(500, {
+                    'Content-Type': 'text/plan;charset=utf-8'
+                });
+                // 中文
+                response.end('500 服务器挂了')
+                return;
+            }
+            response.statusCode = 200;
+            response.setHeader('Content-Type', 'text/html')
+            response.end(data)
+        })
+    } else {
+        response.statusCode = 404
+        response.setHeader('Content-Type', 'text/plan;charset=utf-8');
+        response.end('404 没有这个')
+    }
+
+}).listen(3000, () => {
+    console.log('Server at 3000')
+});
+
+
+// Java
+
+function getPrototypeChain(obj) {
+    const protoChain = [];
+    while(obj = Object.getPrototypeOf(obj)) {
+        protoChain.push(obj)
+    }
+    return protoChain
+}
+
+
