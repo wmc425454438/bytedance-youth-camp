ff63804b320776076f69570afb8879c754b7be15
wmc425454438 <superwong0827@gmail.com>
2021-09-21 21:08:08 +0800

    fix: file stream

--- day01/01_api/05_http.js
+++ day01/01_api/05_http.js
@@ -11,7 +11,7 @@ http.createServer((request, response) => {
     // response.body = 'heiheihei';
 
 
-    const { url , method, headers } = request
+    const { url , method, headers } = request;
     if (url === '/' && method === 'GET') {
         fs.readFile('index.html', (err, data) => {
             if(err) {
@@ -23,10 +23,10 @@ http.createServer((request, response) => {
                 return;
             }
             response.statusCode = 200;
-            response.setHeader('Content-Type', 'text/html')
-            response.end(data)
+            response.setHeader('Content-Type', 'text/html');
+            response.end(data);
         })
-    } else if (url === '/suers' && method === 'GET') {
+    } else if (url === '/users' && method === 'GET') {
         response.writeHead(200, { 'Content-Type': 'application/json' });
         response.end(JSON.stringify({ 'name': 'tom' }))
     } else if (method === 'GET' && headers.accept.indexOf('image/*') !== -1) {
