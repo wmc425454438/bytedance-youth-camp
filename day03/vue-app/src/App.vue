5e48fdc053bad7d8e4a53b3b1d825f13db8bbfb9
wmc425454438 <superwong0827@gmail.com>
2021-09-24 00:49:32 +0800

    feat: antd pro

--- day03/vue-app/src/App.vue
+++ day03/vue-app/src/App.vue
@@ -1,19 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld>
-    Welcome to Your Vue.js App
-  </HelloWorld>
+  <div id="container">
+    <div id="sidebar">
+      <span>Row</span>
+      <span>Col</span>
+      <span>Button</span>
+    </div>
+    
+    <div id="editpanel">
+      <div class="row"></div>
+      <div class="col"></div>
+      <div class="button"></div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+  },
+};
 </script>
 
 <style>
@@ -23,6 +30,38 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  box-sizing: border-box;
+  position: absolute;
+}
+#container {
+  display: flex;
+  width: 100%;
+  height: 100%;
+}
+#sidebar {
+  width: 160px;
+  background-color: #eee;
+}
+#sidebar > * {
+  width: 60px;
+  height: 60px;
+  display: inline-block;
+  margin: 10px;
+  box-sizing: border-box;
+  border: solid 1px #000;
+  user-select: none;
+}
+#editpanel {
+  flex: 1;
+}
+
+.row {
+  width: 100%;
+  min-height: 300px;
+  background-color: blue;
 }
 </style>
